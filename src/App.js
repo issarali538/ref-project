509541addb83172abe931fb7ef9cc39bd2a8b910
issarali538 <you@example.com>
2021-12-09 08:04:01 +0500

    my sentece changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.css";
 function App() {
   return (
     <div className="App">
-      
+      <h1>The quick brown fox jumps over the lazy dogs</h1>
     </div>
   );
 }
